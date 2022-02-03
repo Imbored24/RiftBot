2ea2e8889dfa498dd469a19ac0a24d3a1d6dc95a
knifeworm <98216468+Imbored24@users.noreply.github.com>
2022-02-03 20:22:10 +0000

    Update bot.py, This will allow us to connect to our mongo db from our connection string in the config file.

--- bot.py
+++ bot.py
@@ -2,11 +2,22 @@ import nextcord
 from nextcord.ext import commands
 import config
 import os
+import motor.motor_asyncio
+from utils.mongo import Document
 bot = commands.Bot(command_prefix=config.PREFIX)
 
 @bot.event
 async def on_ready():
     print(f"Bot is online! \nId: {config.BOT_ID}\nUsername: {config.BOT_NAME}")
+    await bot.change_presence(
+        activity=nextcord.Game(name=f"Hi, my names {bot.user.name}.Use {config.PREFIX} to interact with me!")
+    )
+    bot.mongo = motor.motor_asyncio.AsyncIOMotorClient(str(config.MONGODB))
+    bot.db = bot.mongo["myFirstDatabase"]
+    bot.config = Document(bot.db, "configs")
+    print("Database has been loaded!")
+    for document in await bot.config.get_all():
+        print(document)
 
 @bot.command()
 async def load(ctx, extension):
