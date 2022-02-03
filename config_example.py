546f63ac20e8e01221c48dc9cc22b9bbe8cd37ba
knifeworm <98216468+Imbored24@users.noreply.github.com>
2022-02-03 19:29:08 +0000

    Modified config_example.py

--- config_example.py
+++ config_example.py
@@ -1,1 +1,4 @@
 # Please remove the _example from the filename for the bot to work.
+TOKEN="" # Get this token from your discord.com/developers portal after you have created your bot application
+PREFIX="!" # This is what all commands will start with. e.g !ban
+MONGODB="" # To get this please create an account at mongodb atlas and create a free server and the get the login token. A tutorial can be found about this online.
